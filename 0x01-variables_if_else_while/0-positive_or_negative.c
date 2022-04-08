725d2b4944c93e47f3517aa7c5effcd49adf0300
Lazaro Mamayi <lazarusmamai@gmail.com>
2022-04-08 12:20:41 -0700

    Variables, IF, Else, While

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ *main - prints a string on the console
+ *
+ * Return: o when all goes well.
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n",n);
+	else if (n == 0)
+		printf("%d is zero\n",n);
+	else
+		printf("%d is negative\n",n);
+	/* your code goes there */
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+### This is my second low level programming language
