e2e9f55c36e1ed406c3f0dd8d96981e93af194a1
Lazaro Mamayi <lazarusmamai@gmail.com>
2022-04-08 18:09:49 -0700

    Added more alpahbets

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the digits of the alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*Declarations*/
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+	printf("%d", digit);
+	}
+	putchar('\n');
+	return (0);
+}
