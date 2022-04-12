f62010f2350a760350a86bc9f0f96df3f6715d09
Lazaro Mamayi <lazarusmamai@gmail.com>
2022-04-12 12:59:13 -0700

    Added Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_alphabet - Entry Point
+*
+*Return: 0
+*/
+
+void print_alphabet(void)
+{
+	char mychar;
+
+	for (mychar = 'a'; mychar <= 'z'; mychar++)
+	{
+	_putchar(mychar);
+	}
+_putchar('\n');
+}
