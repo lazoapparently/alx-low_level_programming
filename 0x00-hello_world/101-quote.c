5060c8b1e0d2b6ba63c861597f3775267863b49a
lazoapparently <lazarusmamai@gmail.com>
2022-06-09 20:52:07 +0000

    Updated the Quote on Advanced Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
-
+#include <stdlib.h>
 /*
 * main - Function begins
 *
-* Return: 1
+* Return: Always 1
 */
-
 int main(void)
-
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
